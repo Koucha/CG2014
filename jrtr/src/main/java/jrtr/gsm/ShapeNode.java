194b6adec56559e64d2d20bee1e67dcbdcc2e8b2
Florian <florian.joerg@students.unibe.ch>
2014-11-27 00:04:56 +0100

    bounding sphere test WIP

--- jrtr/src/main/java/jrtr/gsm/ShapeNode.java
+++ jrtr/src/main/java/jrtr/gsm/ShapeNode.java
@@ -1,6 +1,8 @@
 package jrtr.gsm;
 
 import javax.vecmath.Matrix4f;
+import javax.vecmath.Vector3f;
+import javax.vecmath.Vector4f;
 
 import jrtr.RenderItem;
 import jrtr.Shape;
@@ -35,6 +37,14 @@ public class ShapeNode extends Leaf
 	{
 		if(classreference.isAssignableFrom(RenderItem.class))
 		{
+			if(ShapeNodeRequestData.class.isAssignableFrom(nodeRequestData.getClass()))
+			{
+				if(isBoundingSphereIntersecting(nodeRequestData.getTF(), ((ShapeNodeRequestData)nodeRequestData).getGsm()) == false)
+				{
+					return false;
+				}
+			}
+			
 			return true;
 		}else
 		{
@@ -47,6 +57,14 @@ public class ShapeNode extends Leaf
 	{
 		if(classreference.isAssignableFrom(RenderItem.class))
 		{
+			if(ShapeNodeRequestData.class.isAssignableFrom(nodeRequestData.getClass()))
+			{
+				if(isBoundingSphereIntersecting(nodeRequestData.getTF(), ((ShapeNodeRequestData)nodeRequestData).getGsm()) == false)
+				{
+					return null;
+				}
+			}
+			
 			@SuppressWarnings("unchecked")
 			T temp = (T) makeRenderItem(tfMatOnStack);
 			return temp;
@@ -56,6 +74,72 @@ public class ShapeNode extends Leaf
 		}
 	}
 	
+	/**
+	 * 
+	 * @param transformationToWorld objToWorld transformation matrix
+	 * @param gsm GraphSceneManager containing Camera and Frustum (World -> CamSpace -> Canonic view vol)
+	 * @return true if the Sphere intersects the view volume
+	 */
+	private boolean isBoundingSphereIntersecting(Matrix4f transformationToWorld, GraphSceneManager gsm)
+	{
+		Matrix4f canonToObjVert = new Matrix4f();
+		canonToObjVert.setIdentity();
+		canonToObjVert.mul(transformationToWorld);	//obj->world
+		canonToObjVert.mul(gsm.getCamera().getCameraMatrix());	//obj->cam
+		canonToObjVert.mul(gsm.getFrustum().getProjectionMatrix());	//obj->canonic
+		
+		Matrix4f canonToObjNormal = new Matrix4f(canonToObjVert);
+		canonToObjNormal.transpose();	//transpose(invert(canon->obj))
+		
+		try {
+			canonToObjVert.invert();	//canon->obj
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return true;
+		}
+		
+		if(isBoundingSphereOverPlane(new Vector4f(1,0,0,1), new Vector4f(1,0,0,0), canonToObjVert, canonToObjNormal) ||
+		   isBoundingSphereOverPlane(new Vector4f(0,1,0,1), new Vector4f(0,1,0,0), canonToObjVert, canonToObjNormal) ||
+		   isBoundingSphereOverPlane(new Vector4f(0,0,1,1), new Vector4f(0,0,1,0), canonToObjVert, canonToObjNormal) ||
+		   isBoundingSphereOverPlane(new Vector4f(-1,0,0,1), new Vector4f(-1,0,0,0), canonToObjVert, canonToObjNormal) ||
+		   isBoundingSphereOverPlane(new Vector4f(0,-1,0,1), new Vector4f(0,-1,0,0), canonToObjVert, canonToObjNormal) ||
+		   isBoundingSphereOverPlane(new Vector4f(0,0,-1,1), new Vector4f(0,0,-1,0), canonToObjVert, canonToObjNormal) )
+		{
+			return false;	// Sphere is outside at least one plane
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * Tests if the bounding sphere is above the given plane
+	 * @param vertex some point on the plane (canonic view volume coordinates)
+	 * @param normal normal of the plane (canonic view volume coordinates)
+	 * @param canonToObjVert canonic view volume -> object space transformation matrix
+	 * @param canonToObjNormal normal transformation matrix (= transpose(invert(canonToObjVert)) )
+	 * @return true if above else false
+	 */
+	private boolean isBoundingSphereOverPlane(Vector4f vertex, Vector4f normal, Matrix4f canonToObjVert, Matrix4f canonToObjNormal)
+	{
+		canonToObjVert.transform(vertex);
+		canonToObjNormal.transform(normal);
+		normal.w = 0;
+		normal.normalize();
+		Vector3f fuckingNormal = new Vector3f(normal.x, normal.y, normal.z);
+		
+		float signedDistancePlaneOrigin = vertex.dot(normal);
+		float signedDistanceSphereCenterOrigin = shape.getBoundingSphereCenter().dot(fuckingNormal);
+		
+		if(signedDistanceSphereCenterOrigin - signedDistancePlaneOrigin > shape.getBoundingSphereRadius())
+		{
+			return false;	//lies above plane
+		}else
+		{
+			return false;	//lies beyond plane
+		}
+	}
+
 	protected RenderItem makeRenderItem(Matrix4f transformationToWorld)
 	{
 		return new RenderItem(shape, transformationToWorld);
